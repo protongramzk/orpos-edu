b11406b11c4547a0cec586e87af2b8314b108917
protongramzk <gunturnina05@gmail.com>
2026-06-09 22:06:32 +0800

    Add UUD Ayat

--- src/lib/data/quizabcd.js
+++ src/lib/data/quizabcd.js
@@ -3,44 +3,19 @@ import { QuizNegara } from './abcd/quiz-negara';
 import { QuizTeknologi } from './abcd/quiz-teknologi';
 import { QuizSains } from './abcd/quiz-sains';
 import {QuizUUD} from './abcd/QuizUUD';
+import { QuizUUD1945 } from './abcd/QuizUUD1'; // Pasal 1-18
+import { QuizUUD1945_Part2 } from './abcd/QuizUUD2'; // Pasal 19-27  
+import { QuizUUD1945_Part3 } from './abcd/QuizUUD3'; // Pasal 28-37
 import { NegaraIbuKota, generateNegaraQuestion } from './abcd/data-negara-ibukota';
 
 export const QuizABCDDB = [
-    {
-        id: "sejarah",
-        title: "SEJARAH AI",
-        levels: [
-            {
-                id: 1,
-                questions: [
-                    {
-                        q: "Siapa pencetus istilah AI?",
-                        a: ["Alan Turing", "John McCarthy", "Bill Gates", "Tesla"],
-                        t: 1
-                    },
-                    {
-                        q: "Turing Test dibuat oleh?",
-                        a: ["Alan Turing", "Newton", "Einstein", "Jobs"],
-                        t: 0
-                    }
-                ]
-            },
-            {
-                id: 2,
-                questions: [
-                    {
-                        q: "Deep Blue milik?",
-                        a: ["Google", "IBM", "Intel", "AMD"],
-                        t: 1
-                    }
-                ]
-            }
-        ]
-    },
     QuizNegara,
     QuizTeknologi,
     QuizSains,
-    QuizUUD
+    QuizUUD,
+    QuizUUD1945,
+    QuizUUD1945_Part2,
+    QuizUUD1945_Part3
 ];
 
 export { NegaraIbuKota, generateNegaraQuestion };
